5f99a8281c1f06495d8180836b07c945daa2ab1d
Ramesh Balaji <nonuser@pop-os.localdomain>
2018-05-16 17:20:18 -0400

    removed src

--- src/BackupUtilities/copy.py
+++
@@ -1,8 +0,0 @@
-import shutil
-import os
-
-def copy(source_file, target_dir):
-    shutil.copyfile(source_file, target_dir)
-
-def rename(source_file, target_file):
-    os.rename(source_file, target_file)
