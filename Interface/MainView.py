2321f2daaeece66a0d48416b9f905710fe8313c4
Ramesh Balaji <nonuser@pop-os.localdomain>
2018-05-19 23:31:41 -0400

    working MainView and Preferences basis. they could be better though.

--- Interface/MainView.py
+++ Interface/MainView.py
@@ -3,10 +3,16 @@ gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk
 
+import Preferences
+class Handlers:
+    def preferences_open(self, widget):
+        Preferences.run()
+
 builder = Gtk.Builder()
 builder.add_from_file("MainView.glade")
-
+builder.connect_signals(Handlers())
 
 window = builder.get_object("mainwin")
+builder.get_object('mainwin').connect('destroy', Gtk.main_quit)
 window.show_all()
 Gtk.main()
--- Interface/Preferences.py
+++ Interface/Preferences.py
@@ -6,10 +6,11 @@ from gi.repository import Gtk
 import sys
 import os
 sys.path.insert(0, os.path.abspath('..'))
+
 from Configuration import config
 
 class Handlers:
-    def entry1validat(self, widget):
+    def entry1validate(self, widget):
         print(builder.get_object("entry").get_text())
 
     def entry2validate(self, widget):
@@ -35,16 +36,18 @@ class Handlers:
         dialog.destroy()
 
 
-builder = Gtk.Builder()
-builder.add_from_file("Preferences.glade")
+def run():
+    builder = Gtk.Builder()
+    builder.add_from_file("Preferences.glade")
 
-builder.connect_signals(Handlers())
-builder.get_object("btn3").connect('clicked', Gtk.main_quit)
-builder.get_object("prefswin").connect('destroy', Gtk.main_quit)
-builder.get_object("entry").set_text(config.get_value("folderstobak"))
-window = builder.get_object("prefswin")
-window.show_all()
+    builder.connect_signals(Handlers())
+    builder.get_object("btn3").connect('clicked', Gtk.main_quit)
+    builder.get_object("prefswin").connect('destroy', Gtk.main_quit)
+    builder.get_object("entry").set_text(config.get_value("folderstobak"))
+    window = builder.get_object("prefswin")
+    window.show_all()
 
-Gtk.main()
+    Gtk.main()
 
-
+if __name__ == "__main__":
+    run()
