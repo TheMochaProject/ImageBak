4949d8ab03452490ee5f6fb02d2dc49e00b0314a
Ramesh Balaji <post2rb@hotmail.com>
2018-06-05 11:50:44 -0400

    restore mainview integratoin

--- Interface/MainView.py
+++ Interface/MainView.py
@@ -10,17 +10,25 @@ sys.path.insert(0, os.path.abspath('..'))
 from BackupUtilities import backup
 
 import Preferences
+from RestoreDialog import RestoreDialog
 
 class Handlers:
     def preferences_open(self, widget):
         Preferences.run()
+
     def backup_now(self, widget):
         builder.get_object('info').set_label("Backing up..")
         backup_obj = backup.Backup()
         backup_obj.backup()
         builder.get_object('info').set_label("Backup complete!\nPress Restore to select and restore from a backup!")
+
     def restore_now(self, widget):
-        pass
+        dialog = RestoreDialog(builder.get_object("mainwin"))
+        response = dialog.run()
+        selected_backup = dialog.get_selected_backup()
+        dialog.destroy()
+        print("DEBUG: Selected backup dir is: " + selected_backup)
+
 
 builder = Gtk.Builder()
 builder.add_from_file("MainView.glade")
