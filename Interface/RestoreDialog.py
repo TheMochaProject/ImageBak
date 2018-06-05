483909ddcb8c4e3ff6ea6b609182574ea8abf6b9
Ramesh Balaji <post2rb@hotmail.com>
2018-06-04 21:04:41 -0400

    working restore dialog

--- Interface/RestoreDialog.py
+++ Interface/RestoreDialog.py
@@ -12,14 +12,14 @@ import glob
 
 class RestoreDialog(Gtk.Dialog):
     def __init__(self, parent):
-        Gtk.Dialog.__init__(self, "My Dialog", parent, 0,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-             Gtk.STOCK_OK, Gtk.ResponseType.OK))
-        self.set_default_size(600,300)
+        Gtk.Dialog.__init__(self, "Select Backup to Restore", parent, 0)
+        self.set_default_size(600,150)
 
         self.response_vbox = Gtk.VBox(spacing=10)
+        self.selected_backup = ""
+        self.response_boxes = {}
+        self.button_mapper = {}
 
-        self.response_boxes = []
         print(config.get_value('backuploc'))
         for folder in glob.glob(config.get_value('backuploc') + "/*"):
             folder_name_array = folder.split("_")
@@ -27,31 +27,33 @@ class RestoreDialog(Gtk.Dialog):
             backup_box = Gtk.Box(spacing = 10)
             backup_label = Gtk.Label(readable_name)
             select_button = Gtk.Button("Select")
+            select_button.connect('clicked', self.log_selected_backup)
             backup_box.pack_start(backup_label, True, True, 0)
             backup_box.pack_start(select_button, True, True, 1)
-            self.response_boxes.append(backup_box)
+            self.response_boxes[backup_box] = folder
+            self.button_mapper[select_button] = folder
         box_index = 0
-        for box in self.response_boxes:
+        for box,folder in self.response_boxes.items():
             self.vbox.pack_start(box, True, True, box_index)
             box_index += 1
         put = self.get_content_area()
         put.add(self.response_vbox)
         self.show_all()
-
+    def log_selected_backup(self, widget):
+        self.selected_backup = self.button_mapper.get(widget)
+        self.destroy()
+    def get_selected_backup(self):
+            return self.selected_backup
 class TestWin(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self,title="testwin")
         dialog = RestoreDialog(self)
         response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("The OK button was clicked")
-        elif response == Gtk.ResponseType.CANCEL:
-            print("The Cancel button was clicked")
-
+        print(dialog.get_selected_backup())
         dialog.destroy()
 
-
-win = TestWin()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+if __name__ == '__main__':
+    win = TestWin()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
