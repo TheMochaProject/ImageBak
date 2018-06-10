108a0f51e3d7ce65c9b60d1a9a7299557e2e7f52
Ramesh Balaji <post2rb@hotmail.com>
2018-06-09 20:27:22 -0400

    setuptools _finally_ works

--- build/lib/src/__main__.py
+++ build/lib/src/__main__.py
@@ -1,4 +1,5 @@
 from Interface import MainView
+
 def main():
     MainView.main()
 if __name__=='__main__':
--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
 import setuptools
+import atexit
+import sys
+import os
+from setuptools.command.install import install
+#boilerplate code from https://stackoverflow.com/questions/20288711/post-install-script-with-python-setuptools
+class CustomInstall(install):
+    def run(self):
+        def _post_install():
+            def find_module_path():
+                print("DEBUG: sys.path is: " + str(sys.path))
+                for p in sys.path[1:]:
+                    if "ImageBak" in p:
+                        return os.path.join(p, "src")
+            install_path = find_module_path()
+            print("Executing post-install scripts...\nReplacing the existing ImageBak script...")
+            print("DEBUG: install_path is " + install_path)
+            with open("/usr/local/bin/ImageBak", 'w') as imagebak_script:
+                imagebak_script.write("#!/bin/sh\npython3 " + install_path + "/__main__.py")
+        atexit.register(_post_install)
+        install.run(self)
 
 setuptools.setup(
     name="ImageBak",
@@ -14,6 +34,7 @@ setuptools.setup(
             'ImageBak=src.__main__:main'
         ]
     },
+    cmdclass={'install': CustomInstall}
 
 
 
